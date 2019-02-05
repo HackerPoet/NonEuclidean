a725c467651f5944d940eaf4e67183b4aa0f0b40
freischj <freischj@oregonstate.edu>
2019-02-04 22:13:18 -0800

    unit testing started and config

---
+++ NonEuclideanTesting/PhysicalTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../NonEuclidean/Player.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NonEuclideanTesting
+{		
+	TEST_CLASS(Player)
+	{
+	public:
+		
+		TEST_METHOD(TryPortal)
+		{
+			Player p;
+		}
+
+	};
+}
